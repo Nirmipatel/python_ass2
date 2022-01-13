a4fad2d84f88079ae0f23425468e9914efa32725
Nirmipatel <88831894+Nirmipatel@users.noreply.github.com>
2022-01-13 14:05:19 +0530

    Add files via upload

---
+++ tuple_prac.py
@@ -0,0 +1,47 @@
+# Write a Python program to create a tuple with different data types.
+#Nirmi patel(20CE095)
+#creating a tuple with different data types
+tuple =1,2,3,'N',"shloka"
+tuple2=1,2,3,4,5
+tuple3='A','B','C','D','F'
+tuple4="NIrmi","20ce095","shloka"
+print("tuple is ",tuple)
+print("tuple2 is ",tuple2)
+print("tuple3 is ",tuple3)
+print("tuple4 is ",tuple4)
+
+#Write a Python program to create a tuple with numbers and print one item.
+#Nirmi patel(20CE095)
+#cretae a tuple
+tup=11,22,33,44,55,66
+#printing a element from a tuple
+print("tuple at index 0 is :",tup[0])
+print("tuple at index 0 is :",tup[1])
+print("tuple at index 0 is :",tup[2])
+print("tuple at index 0 is :",tup[3])
+print("tuple at index 0 is :",tup[4])
+print("tuple at index 0 is :",tup)
+
+#Write a Python program to add an item in a tuple.
+#Nirmi patel(20CE095)
+#creating a tuple
+student="Nirmi","shloka","sajeda","arva","shloka"
+new="rajesh",
+#adding a element to the tuple
+add=student+new
+print("adding element to the tuple",add)
+
+#Write a Python program to convert a tuple to a string.
+#Nirmi Patel(20CE095)
+
+string ="N","i","r","m","i"
+#convert the tuple to the string
+join_string="".join(string)
+print("convert the tuple to the string :",join_string)
+
+#Write a Python program to find the length of a tuple.
+#Nirmi patel(20CE095)
+
+tup2="nirmi","sajeda",1,2,3,'A'
+#find the length of the tuple
+print("The length of the tuple :",len(tup2))
